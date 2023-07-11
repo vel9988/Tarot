21bbe56ec59b6a831f9812eb4e8e53da47cd8f9b
Dmitriy <58105647+vel9988@users.noreply.github.com>
2023-07-11 11:33:20 +0600

    Initial Commit

---
+++ Tarot/TarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotApp.swift
+//  Tarot
+//
+//  Created by Dmitryi Velko on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
