a2fce1657b1e088133787f8ac7ae7ea3ce6a5806
Dmitriy <58105647+vel9988@users.noreply.github.com>
2023-08-03 16:21:39 +0600

    added new cards

--- Tarot/Model/Cards.swift
+++ Tarot/Model/Cards.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Cards {
     
-        let tarotCardNames = [
+    let tarotCardNames = [
         // Major Arcana (22 cards)
         "The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
         "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
@@ -17,25 +17,54 @@ struct Cards {
         "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
         "Judgment", "The World",
         
+        // Major Arcana Reversed (22 cards)
+        "The Fool Reversed", "The Magician Reversed", "The High Priestess Reversed", "The Empress Reversed",
+        "The Emperor Reversed", "The Hierophant Reversed", "The Lovers Reversed", "The Chariot Reversed",
+        "Strength Reversed", "The Hermit Reversed", "Wheel of Fortune Reversed", "Justice Reversed",
+        "The Hanged Man Reversed", "Death Reversed", "Temperance Reversed", "The Devil Reversed",
+        "The Tower Reversed", "The Star Reversed", "The Moon Reversed", "The Sun Reversed",
+        "Judgment Reversed", "The World Reversed",
+        
         // Wands (14 cards)
         "Ace of Wands", "Two of Wands", "Three of Wands", "Four of Wands", "Five of Wands",
         "Six of Wands", "Seven of Wands", "Eight of Wands", "Nine of Wands", "Ten of Wands",
         "Page of Wands", "Knight of Wands", "Queen of Wands", "King of Wands",
+        
+        // Wands Reversed (14 cards)
+        "Ace of Wands Reversed", "Two of Wands Reversed", "Three of Wands Reversed", "Four of Wands Reversed", "Five of Wands Reversed",
+        "Six of Wands Reversed", "Seven of Wands Reversed", "Eight of Wands Reversed", "Nine of Wands Reversed", "Ten of Wands Reversed",
+        "Page of Wands Reversed", "Knight of Wands Reversed", "Queen of Wands Reversed", "King of Wands Reversed",
         
         // Cups (14 cards)
         "Ace of Cups", "Two of Cups", "Three of Cups", "Four of Cups", "Five of Cups",
         "Six of Cups", "Seven of Cups", "Eight of Cups", "Nine of Cups", "Ten of Cups",
         "Page of Cups", "Knight of Cups", "Queen of Cups", "King of Cups",
         
+        // Cups Reversed (14 cards)
+        "Ace of Cups Reversed", "Two of Cups Reversed", "Three of Cups Reversed", "Four of Cups Reversed", "Five of Cups Reversed",
+        "Six of Cups Reversed", "Seven of Cups Reversed", "Eight of Cups Reversed", "Nine of Cups Reversed", "Ten of Cups Reversed",
+        "Page of Cups Reversed", "Knight of Cups Reversed", "Queen of Cups Reversed", "King of Cups Reversed",
+        
         // Swords (14 cards)
         "Ace of Swords", "Two of Swords", "Three of Swords", "Four of Swords", "Five of Swords",
         "Six of Swords", "Seven of Swords", "Eight of Swords", "Nine of Swords", "Ten of Swords",
         "Page of Swords", "Knight of Swords", "Queen of Swords", "King of Swords",
         
+        // Swords Reversed (14 cards)
+        "Ace of Swords Reversed", "Two of Swords Reversed", "Three of Swords Reversed", "Four of Swords Reversed", "Five of Swords Reversed",
+        "Six of Swords Reversed", "Seven of Swords Reversed", "Eight of Swords Reversed", "Nine of Swords Reversed", "Ten of Swords Reversed",
+        "Page of Swords Reversed", "Knight of Swords Reversed", "Queen of Swords Reversed", "King of Swords Reversed",
+        
         // Pentacles (14 cards)
         "Ace of Pentacles", "Two of Pentacles", "Three of Pentacles", "Four of Pentacles", "Five of Pentacles",
         "Six of Pentacles", "Seven of Pentacles", "Eight of Pentacles", "Nine of Pentacles", "Ten of Pentacles",
-        "Page of Pentacles", "Knight of Pentacles", "Queen of Pentacles", "King of Pentacles"
+        "Page of Pentacles", "Knight of Pentacles", "Queen of Pentacles", "King of Pentacles",
+        
+        // Pentacles Reversed (14 cards)
+        "Ace of Pentacles Reversed", "Two of Pentacles Reversed", "Three of Pentacles Reversed", "Four of Pentacles Reversed",
+        "Five of Pentacles Reversed", "Six of Pentacles Reversed", "Seven of Pentacles Reversed", "Eight of Pentacles Reversed",
+        "Nine of Pentacles Reversed", "Ten of Pentacles Reversed", "Page of Pentacles Reversed", "Knight of Pentacles Reversed",
+        "Queen of Pentacles Reversed", "King of Pentacles Reversed"
     ]
     
 }
