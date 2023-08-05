38d044ee461dca63740491c67598a72eb2b70310
Dmitriy <58105647+vel9988@users.noreply.github.com>
2023-08-05 12:37:41 +0600

    updated the deletion method

--- Tarot/ViewModel/SavedPredictionsViewModel.swift
+++ Tarot/ViewModel/SavedPredictionsViewModel.swift
@@ -11,30 +11,28 @@ import RealmSwift
 final class SavedPredictionsViewModel: ObservableObject {
     
     @ObservedResults(Prediction.self) var predictionLists
-
+    
     func deletePrediction(with id: ObjectId) {
-        for prediction in predictionLists {
-            if prediction.id == id {
-                delete(prediction)
-                break
-            }
+        for prediction in predictionLists where prediction.id == id {
+            delete(prediction)
+            break
         }
     }
     
-//    func deletePrediction(at indexSet: IndexSet) {
-//        guard let index = indexSet.first else { return }
-//        let prediction = predictionLists[index]
-//        delete(prediction)
-//    }
+    //    func deletePrediction(at indexSet: IndexSet) {
+    //        guard let index = indexSet.first else { return }
+    //        let prediction = predictionLists[index]
+    //        delete(prediction)
+    //    }
     
     private func delete(_ prediction: ObjectBase) {
-            do {
-                let realm = try Realm()
-                try realm.write {
-                    realm.delete(prediction)                }
-            } catch {
-                print("Error deleting prediction: \(error.localizedDescription)")
-            }
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(prediction)                }
+        } catch {
+            print("Error deleting prediction: \(error.localizedDescription)")
         }
+    }
     
 }
